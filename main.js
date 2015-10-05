19f90d7a46ad6f741ae479bed9c21bfde15863f5
Kushal Tirumala <kushaltirumala99@gmail.com>
2015-10-04 17:25:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-var string = "";
+var gestureString = "";
+if (frame.gestures.length > 0) {
+  for (var i = 0; i < frame.gestures.length; i++) {
+    var gesture = frame.gestures[i];
+    gestureString += "Gesture ID: " + gesture.id + ", "
+                  + "type: " + gesture.type + ", "
+                  + "state: " + gesture.state + ", "
+                  + "hand IDs: " + gesture.handIds.join(", ") + ", "
+                  + "pointable IDs: " + gesture.pointableIds.join(", ") + ", "
+                  + "duration: " + gesture.duration + " &micro;s, ";
 
-Leap.loop(function(frame) {
-
-
-  
-})
-
-
-
-// This allows us to move the cat even whilst in an iFrame.
-Leap.loopController.setBackground(true)
+    switch (gesture.type) {
+      case "circle":
+        gestureString += "center: " + vectorToString(gesture.center) + " mm, "
+                      + "normal: " + vectorToString(gesture.normal, 2) + ", "
+                      + "radius: " + gesture.radius.toFixed(1) + " mm, "
+                      + "progress: " + gesture.progress.toFixed(2) + " rotations";
+        break;
+      case "swipe":
+        gestureString += "start position: " + vectorToString(gesture.startPosition) + " mm, "
+                      + "current position: " + vectorToString(gesture.position) + " mm, "
+                      + "direction: " + vectorToString(gesture.direction, 2) + ", "
+                      + "speed: " + gesture.speed.toFixed(1) + " mm/s";
+        break;
+      case "screenTap":
+      case "keyTap":
+        gestureString += "position: " + vectorToString(gesture.position) + " mm, "
+                      + "direction: " + vectorToString(gesture.direction, 2);
+        break;
+      default:
+        gestureString += "unknown gesture type";
+    }
+    gestureString += "<br />";
+  }
+}
